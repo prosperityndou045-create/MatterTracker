93d6de11a00230c89e5cb633d2ad3b38354b6ea5
prosperityndou045-create <prosperityndou045@gmail.com>
2026-09-04 19:41:06 +0200

    Initial Commit

---
+++ Matter Tracker/Matter_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Matter_TrackerApp.swift
+//  Matter Tracker
+//
+//  Created by Prosperity on 4/9/2026.
+//
+
+import SwiftUI
+
+@main
+struct Matter_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
